50a876cb3207502d563c5ec83fdbb4fbafce7496
AndreaParrini <andrea.parrini26@gmail.com>
2024-04-07 12:00:24 +0200

    add card and insert all results of query
    add h3
    add use function date and strtotime to convert format of birthdate

--- index.php
+++ index.php
@@ -9,14 +9,14 @@ $sql =
      JOIN `posts` ON `posts`.`user_id` = `users`.`id`
      JOIN `likes` ON `likes`.`post_id` = `posts`.`id`
      GROUP BY `users`.`id`
-     ORDER BY `total_like` DESC';
+     ORDER BY `total_like`';
 $result = $connection->query($sql);
 
 var_dump($result);
 
-while ($rows = $result->fetch_assoc()) {
+/* while ($rows = $result->fetch_assoc()) {
     var_dump($rows);
-}
+} */
 
 ?>
 
@@ -36,9 +36,9 @@ while ($rows = $result->fetch_assoc()) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
-<body>
+<body class="bg-secondary">
     <header>
-        <nav class="navbar bg-info p-3 mb-2">
+        <nav class="navbar bg-info p-3 mb-3">
             <div class="container">
                 <a class="navbar-brand" href="#">
                     <i class="fa-solid fa-camera fa-2xl" style="color: #000000;"></i>
@@ -47,7 +47,46 @@ while ($rows = $result->fetch_assoc()) {
             </div>
         </nav>
     </header>
-    <main></main>
+    <main>
+        <div class="container">
+            <h3 class="text-info text-center text-uppercase fst-italic mt-3 mb-3">Lista Utenti con il totale Like ricevuti</h3>
+            <div class="row">
+                <?php while ($rows = $result->fetch_assoc()) :
+                    [
+                        'id' => $userID,
+                        'username' => $username,
+                        'email' => $email,
+                        'birthdate' => $birthdate,
+                        'phone' => $phone,
+                        'total_like' => $total_like
+                    ] = $rows;
+                ?>
+                    <div class="col-xs-12 col-md-6 col-xl-4 mb-3">
+                        <div class="card mb-3 h-100" style="max-width: 540px;">
+                            <div class="row g-0 h-100">
+                                <div class="col-4">
+                                    <img src="https://picsum.photos/id/<?= $userID ?>/200/300" class="img-fluid rounded-start h-100 w-100" alt="Immagine profile utente">
+                                </div>
+                                <div class="col-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?= $username; ?></h5>
+                                        <ul class="list-group list-group-flush">
+                                            <li class="list-group-item p-0 pt-2 pb-2"><?= $email; ?></li>
+                                            <li class="list-group-item p-0 pt-2 pb-2"><?= date('d-m-Y', strtotime($birthdate)); ?></li>
+                                            <li class="list-group-item p-0 pt-2 pb-2">Cellulare : <?= $phone; ?></li>
+                                            <li class="list-group-item p-0 pt-2 pb-2">
+                                                <p class="card-text mt-2"><small class="text-body-secondary">Total like : <?= $total_like; ?></small></p>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </main>
     <footer>
         <!-- place footer here -->
     </footer>
