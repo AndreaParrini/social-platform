5c74526d99ae4a45f01215ff7cf7f441e60fc1ae
AndreaParrini <andrea.parrini26@gmail.com>
2024-04-10 01:14:18 +0200

    add commento on file DB.php

--- DB.php
+++ DB.php
@@ -8,14 +8,17 @@ class DB
      */
     public static function Connect()
     {
+        /* definisco tutti i dati necessari per collegarmi al database */
         define("DB_SERVERNAME", "localhost");
         define("DB_USERNAME", "root");
         define("DB_PASSWORD", "root");
         define("DB_NAME", "db_social_platform");
 
+        /* associo a $connection il risultato restuito dalla funzione mysqli() */
         $connection = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
         /* var_dump($connection); */
 
+        /* controllo se nella variabile $connesction c'è un errore e nel caso stampo in pagina l'errore ricevuto */
         if ($connection && $connection->connect_error) {
             echo "Connection failed" . $connection->connect_error;
             die;
