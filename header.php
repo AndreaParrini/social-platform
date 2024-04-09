8b28dec9871ba3bab32db514f8f46e6513791950
AndreaParrini <andrea.parrini26@gmail.com>
2024-04-10 00:27:30 +0200

    add new a link on header to connect objects.php

--- header.php
+++ header.php
@@ -1,10 +1,18 @@
 <header>
     <nav class="navbar bg-info p-3 mb-3">
         <div class="container">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="./index.php">
                 <i class="fa-solid fa-camera fa-2xl" style="color: #000000;"></i>
             </a>
             <h2 class="fst-italic text-uppercase">Social Platform</h2>
+            <ul class="navbar-nav flex-row gap-3">
+                <li class="nav-item">
+                    <a class="nav-link text-dark fs-5" aria-current="page" href="./index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark fs-5" aria-current="page" href="./objects.php">Objects</a>
+                </li>
+            </ul>
         </div>
     </nav>
 </header>
